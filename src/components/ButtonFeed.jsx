41423abf71d1f1b2f812352e092939e2f3a64ed2
Felippe <felippecorrea2010@gmail.com>
2021-06-17 16:22:51 -0300

    FEAT: conseguindo fazer button jogar novamente funcionark

--- src/components/ButtonFeed.jsx
+++ src/components/ButtonFeed.jsx
@@ -1,33 +1,35 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import BtnRanking from '../components/Buttons';
-import ButtonFeed from '../components/ButtonFeed';
+import { Redirect } from 'react-router-dom';
 // import { connect } from 'react-redux';
 
-class Feedback extends Component {
+class ButtonFeed extends Component {
   constructor() {
     super();
+    this.state = {
+      redirect: false,
+    };
     this.redirectLogin = this.redirectLogin.bind(this);
   }
 
   redirectLogin() {
-    const { history } = this.props;
-    history.push('/');
+    this.setState({
+      redirect: true,
+    });
   }
 
   render() {
+    const { redirect } = this.state;
     return (
       <div>
         <button
           type="button"
           data-testid="btn-play-again"
-          onClick={ this.redirectLogin() }
+          onClick={ this.redirectLogin }
         >
           Jogar novamente
         </button>
-        <h1 data-testid="feedback-text">Feedback</h1>
-        <ButtonFeed />
-        <BtnRanking />
+        { redirect && <Redirect /> }
       </div>
     );
   }
@@ -43,10 +45,10 @@ class Feedback extends Component {
 
 // export default connect(mapStateToProps, mapDispatchToProps)(Feedback);
 
-Feedback.propTypes = {
+ButtonFeed.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func,
   }).isRequired,
 };
 
-export default Feedback;
+export default ButtonFeed;
