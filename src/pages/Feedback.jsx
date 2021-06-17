f1ef46ac7b1f3444a20e02cbe505a14c55eef0a0
Felippe <felippecorrea2010@gmail.com>
2021-06-17 15:48:00 -0300

    FEAT: adicionando butao de jogar novamente na tela de feedback

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 // import { connect } from 'react-redux';
 
 class Feedback extends Component {
+  constructor() {
+    super();
+    this.redirectLogin = this.redirectLogin.bind(this);
+  }
+
+  redirectLogin() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
-      <div />
+      <div>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.redirectLogin() }
+        >
+          Jogar novamente
+        </button>
+      </div>
     );
   }
 }
@@ -18,4 +37,11 @@ class Feedback extends Component {
 // };
 
 // export default connect(mapStateToProps, mapDispatchToProps)(Feedback);
+
+Feedback.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
 export default Feedback;
